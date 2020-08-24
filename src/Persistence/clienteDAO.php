2bba85848a5abfec3dd30c9d4c6bbce634f17639
Thiago Luigi <thiagoluigi7@hotmail.com>
2020-08-24 00:10:35 -0300

    :poop: Close #31 Função de incluir cliente

--- src/Persistence/clienteDAO.php
+++ src/Persistence/clienteDAO.php
@@ -5,11 +5,31 @@
         function __construct() {}
 
         function salvar($cliente, $connection) {
+        
+            $nome = $cliente->getNome();
+            echo $nome;
+            $cpf = $cliente->getCpf();
+            $email = $cliente->getEmail();
+            $telefone = $cliente->getTelefone();
+            $senha = $cliente->getSenha();
+            $endereco = $cliente->getEndereco();
 
-            $sql;
+            $sql = "INSERT INTO `cliente`(`nome`, `cpf`, `email`, `telefone`, `senha`, `endereco`) VALUES ('" . $nome     . "','" . 
+                                                                                                                $cpf      . "','" . 
+                                                                                                                $email    . "','" . 
+                                                                                                                $telefone . "','" .
+                                                                                                                $senha    . "','" .
+                                                                                                                $endereco . "')";
+            if($connection->query($sql) === TRUE) {
+                $connection->close();
+                return true;
+            } else {
+                echo "Erro ao inserir cliente: " . $connection->error;
+                $connection->close();
+                return false;
+            }
 
         }
-        
 
     }
 
