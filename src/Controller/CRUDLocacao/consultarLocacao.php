cd376181c6e169d8b2dc3159c687be79de1a9f4f
Matheus Matias <40282042+MatheusMatiasLima@users.noreply.github.com>
2020-08-25 16:13:11 -0300

    :lipstick: Consultar locação

--- src/Controller/CRUDLocacao/consultarLocacao.php
+++ src/Controller/CRUDLocacao/consultarLocacao.php
@@ -11,6 +11,47 @@
 
     if ($resultado->num_rows > 0) {
         echo "
+        <style>
+        table {
+        border-collapse: collapse;
+        width: 100%;
+        }
+
+        th, td {
+        text-align: left;
+        padding: 8px;
+        }
+
+        tr:nth-child(even){background-color: #f2f2f2}
+
+        th {
+        background-color: #cc0000b4;
+        color: black;
+        }
+
+        #interface {
+            position: relative;
+            width: 1300px;
+            height: 600px;
+            margin: auto auto auto auto;
+            box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.315);
+            background-color: white;
+            padding: 10px;
+        }
+        
+        #retangulo {
+            position: absolute;
+            width: 100%;
+            height: 45px;
+            left: 0px;
+            top: 0px;
+            background: #ffd82cfd; 
+             
+        }
+
+    </style>
+    <body>
+    <div id='interface'>
         <table>
             <tr>
                 <th>Nome do cliente</th>
