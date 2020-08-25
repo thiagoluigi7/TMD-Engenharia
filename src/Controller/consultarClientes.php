a2f222f475311056a137186f649d8543fc2dc620
Matheus Matias <40282042+MatheusMatiasLima@users.noreply.github.com>
2020-08-24 23:06:49 -0300

    :lipstick: tabela de clientes

--- src/Controller/consultarClientes.php
+++ src/Controller/consultarClientes.php
@@ -11,6 +11,47 @@
 
     if ($resultado->num_rows > 0) {
         echo "
+        <style>
+            table {
+            border-collapse: collapse;
+            width: 100%;
+            }
+
+            th, td {
+            text-align: left;
+            padding: 8px;
+            }
+
+            tr:nth-child(even){background-color: #f2f2f2}
+
+            th {
+            background-color: #fcde34b2;
+            color: black;
+            }
+
+            #interface {
+                position: relative;
+                width: 1300px;
+                height: 600px;
+                margin: auto auto auto auto;
+                box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.315);
+                background-color: white;
+                padding: 10px;
+            }
+            
+            #retangulo {
+                position: absolute;
+                width: 100%;
+                height: 45px;
+                left: 0px;
+                top: 0px;
+                background: #ffd82cfd; 
+                 
+            }
+
+        </style>
+        <body>
+        <div id='interface'>
         <table>
             <tr>
                 <th>Nome</th>
@@ -21,8 +62,8 @@
                 <th>Endereço</th>
             </tr>";
         while($registro = $resultado->fetch_assoc()) {
-            echo "<tr>";
-            echo    "<td>".$registro['nome']."</td>
+            echo "<tr>
+                    <td>".$registro['nome']."</td>
                     <td>".$registro['cpf']."</td>
                     <td>".$registro['email']."</td>
                     <td>".$registro['telefone']."</td>
@@ -32,6 +73,7 @@
 
         }
     }
+    echo "</div></body>";
     $connection->close();
 
 
