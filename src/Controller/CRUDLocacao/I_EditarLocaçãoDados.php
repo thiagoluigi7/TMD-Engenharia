16cd953b6874f56b168fbdf4ac61116542ca2947
Matheus Matias <40282042+MatheusMatiasLima@users.noreply.github.com>
2020-08-26 12:49:15 -0300

    :lipstick: Placeholder locação

--- src/Controller/CRUDLocacao/I_EditarLocaçãoDados.php
+++ src/Controller/CRUDLocacao/I_EditarLocaçãoDados.php
@@ -28,13 +28,13 @@
                             <h1>Editar Locação</h1>
 
 
-                            <input type='text' name='id' readonly value='"      . $locacao->getId() . "'>
+                            <input type='text' placeholder='ID' name='id' readonly value='"      . $locacao->getId() . "'>
                             <input type='date' name='dataInicial' value='"      . $locacao->getDataInicial(). "'>
                             <input type='date' name='dataFinal' value='"        . $locacao->getDataFinal() . "'>
-                            <input type='text' name='valorTotal' value='"       . $locacao->getValorTotal() . "'>
-                            <input type='text' name='nomeFuncionario' value='"  . $locacao->getNomeFuncionario() ."'>
-                            <input type='text' name='nomeCliente' value='"      . $locacao->getNomeCliente() . "'>  
-                            <input type='text' name='item' value='"             . $locacao->getItem() . "'>    
+                            <input type='text' placeholder='Valor total' name='valorTotal' value='"       . $locacao->getValorTotal() . "'>
+                            <input type='text' placeholder='Nome do funcionário'name='nomeFuncionario' value='"  . $locacao->getNomeFuncionario() ."'>
+                            <input type='text' placeholder='Nome do cliente' name='nomeCliente' value='"      . $locacao->getNomeCliente() . "'>  
+                            <input type='text' placeholder='Item' name='item' value='"             . $locacao->getItem() . "'>    
                             
                             <div id='botao'>
                             <input type='submit' value='Editar' class='botaoEnviar vermelhoClaro'>
